548151311b10fabcc12fb1a7b7621d3a9511d959
suliman <sulimankhan9212@gmail.com>
2024-10-10 10:52:29 +0500

    commont out the js code for user local ip address

--- CRM DEFENDER V7/SugarModules/modules/LS_CRM_Defender/js/util.js
+++ CRM DEFENDER V7/SugarModules/modules/LS_CRM_Defender/js/util.js
@@ -1,6 +1,6 @@
 /**
  * Check for every line of lines array if line[i] is a valid ip address
- * @param {array} lines 
+ * @param {array} lines
  * @return {string} errorString: Error Message. Return an empty string if everything is ok
  */
 function verifyIP (lines) {
@@ -19,7 +19,7 @@ function verifyIP (lines) {
 
 /**
  * Check if the value is integer
- * @param {} value 
+ * @param {} value
  * @return {string} Error Message. Return an empty string if everything is ok
  */
 function isInt(value) {
@@ -28,8 +28,8 @@ function isInt(value) {
 	if (value == null || value==""){
 		return "";
 	} //if null
-	if ( !isNaN(value) && 
-		 parseInt(Number(value)) == value && 
+	if ( !isNaN(value) &&
+		 parseInt(Number(value)) == value &&
 		 !isNaN(parseInt(value, 10))){
 		 if (value>100)
 			errorString += theName + ': '+value+' is too high.\n';
@@ -37,12 +37,12 @@ function isInt(value) {
 	} //if NaN
 	else
 		errorString += theName + ': '+value+' is not a valid input.\n';
-	return errorString;	
+	return errorString;
 }
 
 /**
  * Check if the value is correct email
- * @param {} value 
+ * @param {} value
  * @return {string} Error Message. Return an empty string if everything is ok
  */
 function verifyAddr(recipientAddress) {
@@ -51,14 +51,14 @@ function verifyAddr(recipientAddress) {
 	if (recipientAddress == null || recipientAddress=="")
 		return "";
 	var regexTest = /^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/.test(recipientAddress);
-	if (!regexTest) {  
+	if (!regexTest) {
 		errorString += theName + ': '+recipientAddress+' is not a valid email.\n';
 		return errorString;
 	}//if
 	else{
-		
+
 		return "";
-	}	
+	}
 }
 
 /**
@@ -66,7 +66,7 @@ function verifyAddr(recipientAddress) {
  * @return {string} Error Message. Return an empty string if everything is ok
  */
 function verify(){
-	var message = ""; 
+	var message = "";
 	var lines = document.getElementById('whiteList').value.split('\n');
 	var maxFailedAccesses = document.getElementById('maxFailedAccesses').value;
 	var recipientAddress = document.getElementById('recipientAddress').value;
@@ -80,13 +80,13 @@ $(document).ready(function() {
 	$("#EditView").submit(function(event){
 	if (document.getElementById('cancelbutton').value!=1) {
 		var $result = verify();
-		if($result!="") { 
+		if($result!="") {
 			alert($result);
-			event.preventDefault();  
-		} //if 
+			event.preventDefault();
+		} //if
 	} //if
   }); //EditView submit
-  
+
 // $("#searchLocalNetIP").click(function(){
 // 	var findIP = new Promise(r=>{var w=window,a=new (w.RTCPeerConnection||w.mozRTCPeerConnection||w.webkitRTCPeerConnection)({iceServers:[]}),b=()=>{};a.createDataChannel("");a.createOffer(c=>a.setLocalDescription(c,b,b),b);a.onicecandidate=c=>{try{c.candidate.candidate.match(/([0-9]{1,3}(\.[0-9]{1,3}){3}|[a-f0-9]{1,4}(:[a-f0-9]{1,4}){7})/g).forEach(r)}catch(e){}}});
 // 	var promiseB = findIP.then(function(result) {
@@ -98,30 +98,30 @@ $(document).ready(function() {
 // });
 
 	//searchLocalNetIP function
-	$("#searchLocalNetIP").click(function() {
-		getUserIP(function(ip) {
-			var whiteListField = document.getElementById('whiteList');
-			var currentWhiteListedIps = whiteListField.value.split("\n");
-
-			if (!currentWhiteListedIps.includes(ip)) {
-				if (whiteListField.value.trim() === "") {
-					whiteListField.value = ip;
-				} else {
-					whiteListField.value += "\n" + ip;
-				}
-			}else{
-				alert("Ip address is already exist in this whiteList");
-			}
-		}, function(error) {
-		// alert("Error: " + error);
-			showWebRTCSettingsPopup();
-		});
-	});
+	// $("#searchLocalNetIP").click(function() {
+	// 	getUserIP(function(ip) {
+	// 		var whiteListField = document.getElementById('whiteList');
+	// 		var currentWhiteListedIps = whiteListField.value.split("\n");
+  //
+	// 		if (!currentWhiteListedIps.includes(ip)) {
+	// 			if (whiteListField.value.trim() === "") {
+	// 				whiteListField.value = ip;
+	// 			} else {
+	// 				whiteListField.value += "\n" + ip;
+	// 			}
+	// 		}else{
+	// 			alert("Ip address is already exist in this whiteList");
+	// 		}
+	// 	}, function(error) {
+	// 	// alert("Error: " + error);
+	// 		showWebRTCSettingsPopup();
+	// 	});
+	// });
 
 	$.getJSON("https://jsonip.com/?callback=?", function (data) {
 		window.globalPublicIP = data.ip;
 	}); //getJSON
-	
+
 	$("#searchPublicNetIP").click(function(){
 		var resultIP = document.getElementById('globalIP').value;
 		var whiteListField = document.getElementById('whiteList');
@@ -137,120 +137,120 @@ $(document).ready(function() {
 		alert("IP address already exists in the whitelist.");
 		}
 	}); //searchPublicNetIP
-  
+
 }); //document.ready function
 
 
-function getUserIP(onNewIP, onError) { // onNewIP - your listener function for new IPs, onError - error handler
-    var myPeerConnection = window.RTCPeerConnection || window.mozRTCPeerConnection || window.webkitRTCPeerConnection;
-
-    if (!myPeerConnection) {
-		onError('Your browser does not support WebRTC');
-		return;
-	}
-
-    var pc = new myPeerConnection({
-      iceServers: []
-    }),
-    noop = function() {},
-    localIPs = {},
-    ipRegex = /([0-9]{1,3}(\.[0-9]{1,3}){3}|[a-f0-9]{1,4}(:[a-f0-9]{1,4}){7})/g,
-    key;
-    function iterateIP(ip) {
-		if (!localIPs[ip]) onNewIP(ip);
-		localIPs[ip] = true;
-	}
-
-    // Create a bogus data channel
-    pc.createDataChannel("");
-
-    // Create offer and set local description
-    pc.createOffer().then(function(sdp) {
-    sdp.sdp.split('\n').forEach(function(line) {
-      if (line.indexOf('candidate') < 0) return;
-      line.match(ipRegex).forEach(iterateIP);
-    });
-
-    pc.setLocalDescription(sdp, noop, noop);
-	}).catch(function(reason) {
-		// Error creating offer
-		onError('Failed to create WebRTC offer: ' + reason.message);
-	});
-
-    // Listen for candidate events
-    pc.onicecandidate = function(ice) {
-		if (!ice || !ice.candidate || !ice.candidate.candidate || !ice.candidate.candidate.match(ipRegex)) return;
-		ice.candidate.candidate.match(ipRegex).forEach(iterateIP);
-	};
-
-    // Set a timeout to detect if no IP was found within 2 seconds
-    setTimeout(function() {
-		if (Object.keys(localIPs).length === 0) {
-		onError('Failed to retrieve IP address. Please check your browser settings or extension (e.g., "Anonymize local IPs exposed by WebRTC").');
-		}
-	}, 200); // Adjust timeout duration as necessary
+// function getUserIP(onNewIP, onError) { // onNewIP - your listener function for new IPs, onError - error handler
+//     var myPeerConnection = window.RTCPeerConnection || window.mozRTCPeerConnection || window.webkitRTCPeerConnection;
+//
+//     if (!myPeerConnection) {
+// 		onError('Your browser does not support WebRTC');
+// 		return;
+// 	}
+//
+//     var pc = new myPeerConnection({
+//       iceServers: []
+//     }),
+//     noop = function() {},
+//     localIPs = {},
+//     ipRegex = /([0-9]{1,3}(\.[0-9]{1,3}){3}|[a-f0-9]{1,4}(:[a-f0-9]{1,4}){7})/g,
+//     key;
+//     function iterateIP(ip) {
+// 		if (!localIPs[ip]) onNewIP(ip);
+// 		localIPs[ip] = true;
+// 	}
+//
+//     // Create a bogus data channel
+//     pc.createDataChannel("");
+//
+//     // Create offer and set local description
+//     pc.createOffer().then(function(sdp) {
+//     sdp.sdp.split('\n').forEach(function(line) {
+//       if (line.indexOf('candidate') < 0) return;
+//       line.match(ipRegex).forEach(iterateIP);
+//     });
+//
+//     pc.setLocalDescription(sdp, noop, noop);
+// 	}).catch(function(reason) {
+// 		// Error creating offer
+// 		onError('Failed to create WebRTC offer: ' + reason.message);
+// 	});
+//
+//     // Listen for candidate events
+//     pc.onicecandidate = function(ice) {
+// 		if (!ice || !ice.candidate || !ice.candidate.candidate || !ice.candidate.candidate.match(ipRegex)) return;
+// 		ice.candidate.candidate.match(ipRegex).forEach(iterateIP);
+// 	};
+//
+//     // Set a timeout to detect if no IP was found within 2 seconds
+//     setTimeout(function() {
+// 		if (Object.keys(localIPs).length === 0) {
+// 		onError('Failed to retrieve IP address. Please check your browser settings or extension (e.g., "Anonymize local IPs exposed by WebRTC").');
+// 		}
+// 	}, 200); // Adjust timeout duration as necessary
 }
 
-function showWebRTCSettingsPopup() {
-  // Create the background overlay and modal container
-  var modalBackground = document.createElement('div');
-  modalBackground.classList.add('modal-background');
-
-  var modalContent = document.createElement('div');
-  modalContent.classList.add('modal-content');
-  modalContent.innerHTML = `
-      <h2>Enable WebRTC Setting</h2>
-      <p>To use this feature, you need to enable the 'Anonymize local IPs exposed by WebRTC' flag in your browser settings:</p>
-      <ul>
-        <li style="padding-top: 9px;">
-          For Chrome: <a href="#" onclick="return false;">chrome://flags/#enable-webrtc-hide-local-ips-with-mdns</a>
-          <button onclick="copyToClipboard('chrome://flags/#enable-webrtc-hide-local-ips-with-mdns')">Copy</button>
-        </li>
-        <li style="padding-top: 9px;">
-          For Edge: <a href="#" onclick="return false;">edge://flags/#enable-webrtc-hide-local-ips-with-mdns</a>
-          <button onclick="copyToClipboard('edge://flags/#enable-webrtc-hide-local-ips-with-mdns')">Copy</button>
-        </li>
-        <li style="padding-top: 9px;">
-          For Firefox: Go to the appropriate settings manually.
-        </li>
-      </ul>
-      <button style="margin-top: 15px;" class="button" id="closePopup">Close</button>
-    `;
-
-  // Append the modal and background to the body
-  document.body.appendChild(modalBackground);
-  document.body.appendChild(modalContent);
-  document.body.classList.add('modal-open'); // Prevent scrolling
-
-  // Close the modal when clicking the close button
-  document.getElementById('closePopup').addEventListener('click', function() {
-    closeModal();
-  });
-
-  // Close the modal when clicking outside of the content
-  modalBackground.addEventListener('click', function(e) {
-    if (e.target === modalBackground) {
-      closeModal();
-    }
-  });
-
-  // Close the modal function
-  function closeModal() {
-    document.body.removeChild(modalBackground);
-    document.body.removeChild(modalContent);
-    document.body.classList.remove('modal-open'); // Restore body scrolling
-  }
-}
+// function showWebRTCSettingsPopup() {
+//   // Create the background overlay and modal container
+//   var modalBackground = document.createElement('div');
+//   modalBackground.classList.add('modal-background');
+//
+//   var modalContent = document.createElement('div');
+//   modalContent.classList.add('modal-content');
+//   modalContent.innerHTML = `
+//       <h2>Enable WebRTC Setting</h2>
+//       <p>To use this feature, you need to enable the 'Anonymize local IPs exposed by WebRTC' flag in your browser settings:</p>
+//       <ul>
+//         <li style="padding-top: 9px;">
+//           For Chrome: <a href="#" onclick="return false;">chrome://flags/#enable-webrtc-hide-local-ips-with-mdns</a>
+//           <button onclick="copyToClipboard('chrome://flags/#enable-webrtc-hide-local-ips-with-mdns')">Copy</button>
+//         </li>
+//         <li style="padding-top: 9px;">
+//           For Edge: <a href="#" onclick="return false;">edge://flags/#enable-webrtc-hide-local-ips-with-mdns</a>
+//           <button onclick="copyToClipboard('edge://flags/#enable-webrtc-hide-local-ips-with-mdns')">Copy</button>
+//         </li>
+//         <li style="padding-top: 9px;">
+//           For Firefox: Go to the appropriate settings manually.
+//         </li>
+//       </ul>
+//       <button style="margin-top: 15px;" class="button" id="closePopup">Close</button>
+//     `;
+//
+//   // Append the modal and background to the body
+//   document.body.appendChild(modalBackground);
+//   document.body.appendChild(modalContent);
+//   document.body.classList.add('modal-open'); // Prevent scrolling
+//
+//   // Close the modal when clicking the close button
+//   document.getElementById('closePopup').addEventListener('click', function() {
+//     closeModal();
+//   });
+//
+//   // Close the modal when clicking outside of the content
+//   modalBackground.addEventListener('click', function(e) {
+//     if (e.target === modalBackground) {
+//       closeModal();
+//     }
+//   });
+//
+//   // Close the modal function
+//   function closeModal() {
+//     document.body.removeChild(modalBackground);
+//     document.body.removeChild(modalContent);
+//     document.body.classList.remove('modal-open'); // Restore body scrolling
+//   }
+// }
 
 // Function to copy the text to clipboard
-function copyToClipboard(text) {
-  var tempInput = document.createElement('input');
-  tempInput.style.position = 'absolute';
-  tempInput.style.left = '-9999px';
-  tempInput.value = text;
-  document.body.appendChild(tempInput);
-  tempInput.select();
-  document.execCommand('copy');
-  document.body.removeChild(tempInput);
-  alert('Copied to clipboard: ' + text);
-}
+// function copyToClipboard(text) {
+//   var tempInput = document.createElement('input');
+//   tempInput.style.position = 'absolute';
+//   tempInput.style.left = '-9999px';
+//   tempInput.value = text;
+//   document.body.appendChild(tempInput);
+//   tempInput.select();
+//   document.execCommand('copy');
+//   document.body.removeChild(tempInput);
+//   alert('Copied to clipboard: ' + text);
+// }
--- CRM DEFFENDER V8/SugarModules/modules/LS_CRM_Defender/js/util.js
+++ CRM DEFFENDER V8/SugarModules/modules/LS_CRM_Defender/js/util.js
@@ -87,25 +87,25 @@ $(document).ready(function() {
 	} //if
   }); //EditView submit
 
-	$("#searchLocalNetIP").click(function() {
-		getUserIP(function(ip) {
-			var whiteListField = document.getElementById('whiteList');
-			var currentWhiteListedIps = whiteListField.value.split("\n");
-
-			if (!currentWhiteListedIps.includes(ip)) {
-				if (whiteListField.value.trim() === "") {
-					whiteListField.value = ip;
-				} else {
-					whiteListField.value += "\n" + ip;
-				}
-			}else{
-				alert("Ip address is already exist in this whiteList");
-			}
-		}, function(error) {
-		// alert("Error: " + error);
-			showWebRTCSettingsPopup();
-		});
-	});
+	// $("#searchLocalNetIP").click(function() {
+	// 	getUserIP(function(ip) {
+	// 		var whiteListField = document.getElementById('whiteList');
+	// 		var currentWhiteListedIps = whiteListField.value.split("\n");
+  //
+	// 		if (!currentWhiteListedIps.includes(ip)) {
+	// 			if (whiteListField.value.trim() === "") {
+	// 				whiteListField.value = ip;
+	// 			} else {
+	// 				whiteListField.value += "\n" + ip;
+	// 			}
+	// 		}else{
+	// 			alert("Ip address is already exist in this whiteList");
+	// 		}
+	// 	}, function(error) {
+	// 	// alert("Error: " + error);
+	// 		showWebRTCSettingsPopup();
+	// 	});
+	// });
 
 	$.getJSON("https://jsonip.com/?callback=?", function (data) {
 		window.globalPublicIP = data.ip;
@@ -130,117 +130,117 @@ $(document).ready(function() {
 }); //document.ready function
 
 
-function getUserIP(onNewIP, onError) { // onNewIP - your listener function for new IPs, onError - error handler
-    var myPeerConnection = window.RTCPeerConnection || window.mozRTCPeerConnection || window.webkitRTCPeerConnection;
-
-    if (!myPeerConnection) {
-    onError('Your browser does not support WebRTC');
-    return;
-  }
-
-    var pc = new myPeerConnection({
-      iceServers: []
-    }),
-    noop = function() {},
-    localIPs = {},
-    ipRegex = /([0-9]{1,3}(\.[0-9]{1,3}){3}|[a-f0-9]{1,4}(:[a-f0-9]{1,4}){7})/g,
-    key;
-
-    function iterateIP(ip) {
-    if (!localIPs[ip]) onNewIP(ip);
-    localIPs[ip] = true;
-  }
-
-    // Create a bogus data channel
-    pc.createDataChannel("");
-
-    // Create offer and set local description
-    pc.createOffer().then(function(sdp) {
-    sdp.sdp.split('\n').forEach(function(line) {
-      if (line.indexOf('candidate') < 0) return;
-      line.match(ipRegex).forEach(iterateIP);
-    });
-
-    pc.setLocalDescription(sdp, noop, noop);
-  }).catch(function(reason) {
-    // Error creating offer
-    onError('Failed to create WebRTC offer: ' + reason.message);
-  });
-
-    // Listen for candidate events
-    pc.onicecandidate = function(ice) {
-    if (!ice || !ice.candidate || !ice.candidate.candidate || !ice.candidate.candidate.match(ipRegex)) return;
-    ice.candidate.candidate.match(ipRegex).forEach(iterateIP);
-  };
-
-    // Set a timeout to detect if no IP was found within 2 seconds
-    setTimeout(function() {
-    if (Object.keys(localIPs).length === 0) {
-      onError('Failed to retrieve IP address. Please check your browser settings or extension (e.g., "Anonymize local IPs exposed by WebRTC").');
-    }
-  }, 200); // Adjust timeout duration as necessary
-}
-
-function showWebRTCSettingsPopup() {
-  // Create the background overlay and modal container
-  var modalBackground = document.createElement('div');
-  modalBackground.classList.add('modal-background');
-
-  var modalContent = document.createElement('div');
-  modalContent.classList.add('modal-content');
-  modalContent.innerHTML = `
-      <h2>Enable WebRTC Setting</h2>
-      <p>To use this feature, you need to enable the 'Anonymize local IPs exposed by WebRTC' flag in your browser settings:</p>
-      <ul>
-        <li style="padding-top: 9px;">
-          For Chrome: <a href="#" onclick="return false;">chrome://flags/#enable-webrtc-hide-local-ips-with-mdns</a>
-          <button onclick="copyToClipboard('chrome://flags/#enable-webrtc-hide-local-ips-with-mdns')">Copy</button>
-        </li>
-        <li style="padding-top: 9px;">
-          For Edge: <a href="#" onclick="return false;">edge://flags/#enable-webrtc-hide-local-ips-with-mdns</a>
-          <button onclick="copyToClipboard('edge://flags/#enable-webrtc-hide-local-ips-with-mdns')">Copy</button>
-        </li>
-        <li style="padding-top: 9px;">
-          For Firefox: Go to the appropriate settings manually.
-        </li>
-      </ul>
-      <button class="button" id="closePopup" style="margin-top: 10px;">Close</button>
-    `;
-
-  // Append the modal and background to the body
-  document.body.appendChild(modalBackground);
-  document.body.appendChild(modalContent);
-  document.body.classList.add('modal-open'); // Prevent scrolling
-
-  // Close the modal when clicking the close button
-  document.getElementById('closePopup').addEventListener('click', function() {
-    closeModal();
-  });
-
-  // Close the modal when clicking outside of the content
-  modalBackground.addEventListener('click', function(e) {
-    if (e.target === modalBackground) {
-      closeModal();
-    }
-  });
-
-  // Close the modal function
-  function closeModal() {
-    document.body.removeChild(modalBackground);
-    document.body.removeChild(modalContent);
-    document.body.classList.remove('modal-open'); // Restore body scrolling
-  }
-}
-
-// Function to copy the text to clipboard
-function copyToClipboard(text) {
-  var tempInput = document.createElement('input');
-  tempInput.style.position = 'absolute';
-  tempInput.style.left = '-9999px';
-  tempInput.value = text;
-  document.body.appendChild(tempInput);
-  tempInput.select();
-  document.execCommand('copy');
-  document.body.removeChild(tempInput);
-  alert('Copied to clipboard: ' + text);
-}
+// function getUserIP(onNewIP, onError) { // onNewIP - your listener function for new IPs, onError - error handler
+//     var myPeerConnection = window.RTCPeerConnection || window.mozRTCPeerConnection || window.webkitRTCPeerConnection;
+//
+//     if (!myPeerConnection) {
+//     onError('Your browser does not support WebRTC');
+//     return;
+//   }
+//
+//     var pc = new myPeerConnection({
+//       iceServers: []
+//     }),
+//     noop = function() {},
+//     localIPs = {},
+//     ipRegex = /([0-9]{1,3}(\.[0-9]{1,3}){3}|[a-f0-9]{1,4}(:[a-f0-9]{1,4}){7})/g,
+//     key;
+//
+//     function iterateIP(ip) {
+//     if (!localIPs[ip]) onNewIP(ip);
+//     localIPs[ip] = true;
+//   }
+//
+//     // Create a bogus data channel
+//     pc.createDataChannel("");
+//
+//     // Create offer and set local description
+//     pc.createOffer().then(function(sdp) {
+//     sdp.sdp.split('\n').forEach(function(line) {
+//       if (line.indexOf('candidate') < 0) return;
+//       line.match(ipRegex).forEach(iterateIP);
+//     });
+//
+//     pc.setLocalDescription(sdp, noop, noop);
+//   }).catch(function(reason) {
+//     // Error creating offer
+//     onError('Failed to create WebRTC offer: ' + reason.message);
+//   });
+//
+//     // Listen for candidate events
+//     pc.onicecandidate = function(ice) {
+//     if (!ice || !ice.candidate || !ice.candidate.candidate || !ice.candidate.candidate.match(ipRegex)) return;
+//     ice.candidate.candidate.match(ipRegex).forEach(iterateIP);
+//   };
+//
+//     // Set a timeout to detect if no IP was found within 2 seconds
+//     setTimeout(function() {
+//     if (Object.keys(localIPs).length === 0) {
+//       onError('Failed to retrieve IP address. Please check your browser settings or extension (e.g., "Anonymize local IPs exposed by WebRTC").');
+//     }
+//   }, 200); // Adjust timeout duration as necessary
+// }
+//
+// function showWebRTCSettingsPopup() {
+//   // Create the background overlay and modal container
+//   var modalBackground = document.createElement('div');
+//   modalBackground.classList.add('modal-background');
+//
+//   var modalContent = document.createElement('div');
+//   modalContent.classList.add('modal-content');
+//   modalContent.innerHTML = `
+//       <h2>Enable WebRTC Setting</h2>
+//       <p>To use this feature, you need to enable the 'Anonymize local IPs exposed by WebRTC' flag in your browser settings:</p>
+//       <ul>
+//         <li style="padding-top: 9px;">
+//           For Chrome: <a href="#" onclick="return false;">chrome://flags/#enable-webrtc-hide-local-ips-with-mdns</a>
+//           <button onclick="copyToClipboard('chrome://flags/#enable-webrtc-hide-local-ips-with-mdns')">Copy</button>
+//         </li>
+//         <li style="padding-top: 9px;">
+//           For Edge: <a href="#" onclick="return false;">edge://flags/#enable-webrtc-hide-local-ips-with-mdns</a>
+//           <button onclick="copyToClipboard('edge://flags/#enable-webrtc-hide-local-ips-with-mdns')">Copy</button>
+//         </li>
+//         <li style="padding-top: 9px;">
+//           For Firefox: Go to the appropriate settings manually.
+//         </li>
+//       </ul>
+//       <button class="button" id="closePopup" style="margin-top: 10px;">Close</button>
+//     `;
+//
+//   // Append the modal and background to the body
+//   document.body.appendChild(modalBackground);
+//   document.body.appendChild(modalContent);
+//   document.body.classList.add('modal-open'); // Prevent scrolling
+//
+//   // Close the modal when clicking the close button
+//   document.getElementById('closePopup').addEventListener('click', function() {
+//     closeModal();
+//   });
+//
+//   // Close the modal when clicking outside of the content
+//   modalBackground.addEventListener('click', function(e) {
+//     if (e.target === modalBackground) {
+//       closeModal();
+//     }
+//   });
+//
+//   // Close the modal function
+//   function closeModal() {
+//     document.body.removeChild(modalBackground);
+//     document.body.removeChild(modalContent);
+//     document.body.classList.remove('modal-open'); // Restore body scrolling
+//   }
+// }
+//
+// // Function to copy the text to clipboard
+// function copyToClipboard(text) {
+//   var tempInput = document.createElement('input');
+//   tempInput.style.position = 'absolute';
+//   tempInput.style.left = '-9999px';
+//   tempInput.value = text;
+//   document.body.appendChild(tempInput);
+//   tempInput.select();
+//   document.execCommand('copy');
+//   document.body.removeChild(tempInput);
+//   alert('Copied to clipboard: ' + text);
+// }
